04a0e19def29ebf12d0d31ebfb3dba9e714c0303
kjh0718 <ranbow3062961@gmail.com>
2026-05-15 00:31:29 +0900

    chore: add sample data for user, wallet, post database

--- docker/wallet-db/init.sql
+++ docker/wallet-db/init.sql
@@ -3,3 +3,7 @@ CREATE TABLE wallets (
     user_id   BIGINT NOT NULL UNIQUE,
     balance   INT NOT NULL DEFAULT 0 CHECK (balance >= 0)
 );
+
+INSERT INTO wallets (user_id, balance)
+VALUES  (1, 1000000),
+        (2, 1000000);
